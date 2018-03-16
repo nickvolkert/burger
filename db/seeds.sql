1127eb5b868dc85783a0e5d21891d0dc2fa75305
Nicholas Volkert <nvolkert@NVs-iMac.attlocal.net>
2018-03-16 00:20:48 -0500

    adding all this madness

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgerDB.burgers (name) VALUES ('Black Sabbath');
+INSERT INTO burgerDB.burgers (name, devoured) VALUES ('Megadeth', true);
+INSERT INTO burgerDB.burgers (name, devoured) VALUES ('Skid Row', true);
+INSERT INTO burgerDB.burgers (name) VALUES ('Iron Butterfly');
+
+SELECT * FROM burgerDB.burgers;
